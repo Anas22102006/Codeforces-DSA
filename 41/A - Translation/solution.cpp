ac0a0fa7be01a2f05cbd24457e388e03758a5248
Anas Usman Ali <145250917+Anas22102006@users.noreply.github.com>
2026-03-09 23:30:18 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;int main(){    string s1,s2;    cin >> s1;    cin >> s2;    string r= s2;    reverse(r.begin(),r.end());    if(s1==r){        cout << "YES";    }    else{        cout << "NO";    }    return 0;}
