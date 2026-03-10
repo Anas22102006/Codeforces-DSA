756e9df4eb9fc94f9e6163e9891f4751d3e3a7b2
Anas Usman Ali <145250917+Anas22102006@users.noreply.github.com>
2026-03-11 02:04:01 +0530

    Add Vanya and Lanterns [B] from Codeforces

---
+++ 492/B - Vanya and Lanterns/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main(){    int n, l;    cin >> n >> l;    vector<int> a(n);    for(int i=0;i<n;i++) cin >> a[i];     sort(a.begin(), a.end());     double ans = max(a[0], l - a[n-1]) * 1.0;     for(int i=0;i<n-1;i++){        ans = max(ans, (a[i+1] - a[i]) / 2.0);    }     cout << fixed << setprecision(10) << ans;    return 0;}
